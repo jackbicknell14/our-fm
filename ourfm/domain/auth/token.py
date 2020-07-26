74bba8c1c5e5190a13466a289c352df309f230b9
Jack Bicknell <jackbicknell@gmail.com>
2020-07-26 10:59:47 +0100

    debug logging

--- ourfm/domain/auth/token.py
+++ ourfm/domain/auth/token.py
@@ -2,6 +2,7 @@ from flask import current_app as app
 import urllib
 import requests
 
+logger = app.logger
 
 CLIENT_ID = app.config['CLIENT_ID']
 CLIENT_SECRET = app.config['CLIENT_SECRET']
@@ -29,7 +30,10 @@ def get_refresh_token(authorization_code):
         'client_secret': CLIENT_SECRET
     }
     url = 'https://accounts.spotify.com/api/token'
+    logger.info(data)
     response = requests.post(url, data=data)
+    logger.info(response.url)
+    response.raise_for_status()
     data = response.json()
     refresh_token = data['refresh_token']
     return refresh_token
