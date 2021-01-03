da4f78d942d0ca1f9ba07801c80a7f2fa196ae77
Jack Bicknell <jackbicknell@gmail.com>
2021-01-03 21:45:23 +0000

    Added minute ping

--- ourfm/interfaces/tasks/celery_worker.py
+++ ourfm/interfaces/tasks/celery_worker.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import logging
 from celery.schedules import crontab
+import requests
 from ourfm import celery, create_app
 
 app = create_app(os.getenv('FLASK_ENV'))
@@ -20,6 +21,9 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(minute='0', hour='0', day_of_month='4'),
                              create_monthly_playlists.s(),
                              name='create-monthly-playlists')
+    sender.add_periodic_task(crontab(minute='*'),
+                             minute_ping.s(),
+                             name='minute-ping')
 
 
 @celery.task
@@ -27,5 +31,10 @@ def create_monthly_playlists():
     logging.info('Creating monthly playlists for all users.')
     playlists = spotify.create_playlists()
 #    spotify.send_create_playlist_emails(playlists)
+
+
+@celery.task
+def minute_ping():
+    requests.get('https://ourfm-production.herokuapp.com/check/ok')
 
 
